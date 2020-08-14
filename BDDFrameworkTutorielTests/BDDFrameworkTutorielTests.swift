e53cf04fc2d69daaf2358bf8a4ff999ef023e100
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-08-14 14:09:31 +0200

    Set up Unit tests

--- BDDFrameworkTutorielTests/BDDFrameworkTutorielTests.swift
+++ BDDFrameworkTutorielTests/BDDFrameworkTutorielTests.swift
@@ -10,25 +10,46 @@ import XCTest
 @testable import BDDFrameworkTutoriel
 
 class BDDFrameworkTutorielTests: XCTestCase {
+    
+    var upgrader: PlayerUpgrader!
+    var player: Player!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+        player = Player()
+        upgrader = PlayerUpgrader(player: player)
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+     // MARK: Player Lives
+    func test_addMoreLivesWith0Lives_Adds0NewLivesToPlayer() {
+      try? upgrader.upgradeLives(by: 0)
+      XCTAssertEqual(player.lives, 0)
     }
+    
+    func test_addMoreLivesWithXLives_AddsXNumberOfLivesToPlayer_WhenPlayerStartsWithNoLives() {
+       try? upgrader.upgradeLives(by: 1)
+       XCTAssertEqual(player.lives, 1)
+     }
+     
+     func test_addMoreLivesWithNegativeLives_ThrowsAnError() {
+       XCTAssertThrowsError(try upgrader.upgradeLives(by: -1))
+     }
+     
+     func test_addMoreLivesWithMoreThanMaximumNumberLives_SetsPlayersLivesToMaximumNumberOfLives_WhenPlayerStartsWithNoLives() {
+       try? upgrader.upgradeLives(by: Player.maximumLives+1)
+       XCTAssertEqual(player.lives, Player.maximumLives)
+     }
+     
+     func test_addMoreLives_SetsPlayersLivesToMaximumNumberOfLives_WhenPlayerStartsWithMaximumLives() {
+       try? upgrader.upgradeLives(by: 1)
+       try? upgrader.upgradeLives(by: 1)
+       try? upgrader.upgradeLives(by: 1)
+       try? upgrader.upgradeLives(by: 1)
+       XCTAssertEqual(player.lives, Player.maximumLives)
+     }
+    
+    
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
